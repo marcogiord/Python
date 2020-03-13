840f1358bb7f08e26a7414379f3a5c2db5930c0b
MarcoG <MGiordano@medis.nl>
2020-03-13 13:24:52 +0000

    first test commit
    commit

--- classfication.py
+++ classfication.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print(tf.__version__)
-
+#dfgdfgdfgdfgfdg
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
